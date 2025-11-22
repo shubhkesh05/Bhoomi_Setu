e2e18b77e60a9e334f94bd05a64c43b12612ae34
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-11-22 20:35:18 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,41 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  aadhaarNumber: {
+    type: String,
+    unique: true,
+     sparse: true, // ✅ allows multiple null values
+    match: /^\d{12}$/
+  },
+  
 
-export default mongoose.model('User', userSchema);
+  name: String,
+  phoneNumber: String,
+  role: {
+    type: String,
+    enum: ['citizen', 'panchayat_officer', 'legal_advisor', 'admin' ,'tehsildar' ],
+    default: 'citizen'
+  },
+  languagePreference: {
+    type: String,
+    default: 'en'
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+// ✅ Export as default for ES Module compatibility
+export default User;
