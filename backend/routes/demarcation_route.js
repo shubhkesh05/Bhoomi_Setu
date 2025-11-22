cfa3182750b794e4298b49f3b71d80bc734e82fa
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-11-22 20:40:01 +0530

    Update demarcation_route.js

--- backend/routes/demarcation_route.js
+++ backend/routes/demarcation_route.js
@@ -10,13 +10,10 @@ import {
 
 const router = express.Router();
 
-// Submit a new demarcation request
 router.post("/", protect, createDemarcationRequest);
 
-// Get all demarcation requests of logged-in user
 router.get("/my-requests", protect, getMyDemarcationRequests);
 
-// Get specific demarcation request by ID
 router.get("/:id", protect, getDemarcationRequestById);
 
 export default router;
