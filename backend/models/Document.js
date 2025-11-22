bf9048aff3095e9482cc1aa195533204632edf47
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-11-22 20:19:57 +0530

    Create Document.js

---
+++ backend/models/Document.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const documentSchema = new mongoose.Schema({
+    uploadedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    fileName: String,
+    fileUrl: String,
+    fileType: String,
+    fileSize: Number,
+    fileHash: String,   // Optional for file integrity
+    docType: {
+        type: String,
+        enum: ['identity', 'ownership_proof', 'legal_notice', 'mutation_form', 'map'],
+        required: true
+    },
+    ocrExtract: String,
+    isVerified: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+export default mongoose.model('Document', documentSchema);
