0e60a3fde5060f4405db7aa06110f455feb6936d
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-11-23 19:24:25 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err.message);
-    process.exit(1); // Exit if connection fails
+    process.exit(1); 
   }
 };
 
